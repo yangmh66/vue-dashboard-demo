447ad5d9d5f95e4a71acd2bb5614815dcba5af89
Ming-Hsiao Yang <yangmh66@gmail.com>
2019-07-04 13:37:29 +0800

    upload vue-dashboard-demo chapter.#88

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png" /> -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+  // created() {
+  //   const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`; //"/api/:yangmh66/products";
+  //   console.log(process.env.APIPATH, process.env.CUSTOMPATH);
+  //   this.$http.get(api).then(response => {
+  //     console.log(response.data);
+  //   });
+  // }
+};
+</script>
+
+<style lang="scss">
+@import "./assets/all.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,58 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+//官方&第三方套件
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+//自訂套件
+import App from './App';
+import router from './router';
+
+
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+
+axios.defaults.withCredentials = true;
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+});
+
+router.beforeEach((to, from, next) => {
+
+    console.log('In beforeEach...');
+    console.log('to', to, 'from', from, 'next', next);
+    //
+    if (to.meta.requiresAuth) {
+        console.log('這裡需要驗證!!');
+        // console.log('from', from, 'to', to, 'next', next);
+
+        const api = `${process.env.APIPATH}/api/user/check`;
+
+        axios.post(api).then(response => {
+            console.log(response.data);
+            if (response.data.success) {
+                console.log('驗證成功!!');
+                // console.log('next', next);
+                console.log('from', from, 'to', to, 'next', next);
+                next();
+            } else {
+                console.log('驗證失敗!!');
+                next({
+                    path: '/login',
+                });
+            }
+        });
+    } else {
+        console.log('In else of --> if (to.meta.requiresAuth) ...');
+        console.log('from', from, 'to', to, 'next', next);
+        next();
+    }
+
+});
